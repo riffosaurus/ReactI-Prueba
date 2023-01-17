653d79500db875fa46ab3e37260a66486ab5b90f
sebavp <seba.vegap@gmail.com>
2023-01-17 16:11:52 -0300

    filtro farmacias x region

--- src/components/Farmacias.jsx
+++ src/components/Farmacias.jsx
@@ -1,7 +1,7 @@
 //importa el usestate y useffect
 import React, { useState, useEffect } from "react";
 //importa el componente de bootstrap de contenedor y card
-import { Container, Card, Row, Col} from "react-bootstrap";
+import { Container, Card, Row, Col, Button} from "react-bootstrap";
 
 
 //funcion de componentes farmacias
@@ -34,8 +34,29 @@ console.log(props.valorSelect + ' componente famacias')
 
 
     const renderCard = (card, index) => {
-      //falta ordenar los resultados nuermicamente segun fk_region
-      
+      //la condición filtrará las farmacias de acuerdo al valor del menú dropdown
+      if (card.fk_region === props.valorSelect)
+        return(
+          <Col className="d-flex align-items-stretch col-sm-2 pt-4">
+          <Card key={index} className="bg-secondary w-100">
+            <Card.Body>
+              <Card.Title>{card.local_nombre}</Card.Title>
+              <Card.Text>
+                Comuna de {card.comuna_nombre}
+              </Card.Text>
+              <Card.Text>
+                Dirección: {card.local_direccion}
+              </Card.Text>
+              <Card.Text>
+                Teléfono: {card.local_telefono}
+              </Card.Text>
+              
+              </Card.Body>
+              </Card>
+              </Col>
+        )
+        //esta condición mostrará todas las farmacias cuando el valor del menú dropdown sea vacío
+        else if (props.valorSelect === '')
         return(
           <Col className="d-flex align-items-stretch col-sm-2 pt-4">
           <Card key={index} className="bg-secondary w-100">
@@ -65,12 +86,12 @@ console.log(props.valorSelect + ' componente famacias')
         <h2>Farmacias de turno hoy</h2>
         <Container fluid className=" pt-3">
           <Row className="d-flex align-items-stretch">
-           {/* mostramos todos los resultados que guardamos anteriormente, con map se llama a la función renderCard para cada objeto del array*/}
-           
-           {
-           
+           {/* mostramos todos los resultados que guardamos anteriormente, con map se llama a la función renderCard para cada objeto del array.
+           si el array dentro de info se encuentra vacío, se mostrará el string 'cargando' mientras se espera la respuesta de la API*/}
+           {props.info.length === 0 ? <h3>Cargando...</h3> : 
            props.info.map(renderCard) 
-    }
+    
+  }
             </Row>
         </Container>
     </div>
